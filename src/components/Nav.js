6ba00e75cab9afa456fd5f377c19c18373cff9c9
Feliverse <114632980+Feliverse@users.noreply.github.com>
2023-03-04 16:44:23 -0400

    adjust link in Nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,8 +7,8 @@ function Nav() {
         Bookstore CMS
       </h1>
       <nav className="options">
-        <Link to="/Books">BOOKS</Link>
-        <Link to="Categories">CATEGORIES</Link>
+        <Link to="/"> [BOOKS] </Link>
+        <Link to="Categories"> [CATEGORIES] </Link>
       </nav>
     </div>
   );
