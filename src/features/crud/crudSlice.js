10581ecc050d5c2091472653cd42c8f44a99d373
Feliverse <114632980+Feliverse@users.noreply.github.com>
2023-03-06 20:56:02 -0400

    Ready for reducers

---
+++ src/features/crud/crudSlice.js
@@ -0,0 +1,11 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const crudSlice = createSlice({
+  name: 'tasks',
+  initialState: [],
+  reducers: {
+    
+  },
+});
+
+export default crudSlice;
