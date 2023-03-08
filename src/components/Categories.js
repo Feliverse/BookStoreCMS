e60461b32bd72372509437cb083dad0a28802c2c
Feliverse <114632980+Feliverse@users.noreply.github.com>
2023-03-08 17:11:37 -0400

    clean extra line in categories

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { checkstatus } from '../redux/categories/categoriesSlice';
 
-
 function Categories() {
   const category = useSelector((state) => state.categories.categories);
   const dispatch = useDispatch();
