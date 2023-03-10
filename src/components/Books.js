1bbea343e868a659056c938f0c0e4da019ef580a
Feliverse <114632980+Feliverse@users.noreply.github.com>
2023-03-09 20:56:53 -0400

    Use axios with createAsyncThunk to API calls

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,11 +1,20 @@
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import Book from './Book';
 import Form from './Form';
+import { fetchBooks } from '../redux/books/bookSlice';
 
 function Books() {
-  const { books } = useSelector((store) => store.books);
+  const { books, isLoading } = useSelector((store) => store.books);
+  const dispatch = useDispatch();
+
+  useEffect(() => { dispatch(fetchBooks()); }, []);
+
   return (
     <section className="forBooks">
+      <div>
+        {isLoading && <h1 style={{ textAlign: 'center', color: 'blue' }}>Loading...</h1>}
+      </div>
       {books.map((book) => (
         <Book
           key={book.item_id}
